d2cbee580ae9d71dd67268723e970839b882d176
yagnapraneeth <119563035+yagnapraneeth@users.noreply.github.com>
2023-05-20 14:22:33 +0530

    Create 12345.java

---
+++ 12345.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+	    int i;int j;
+	    for(i=0;i<3;i++)
+	    {
+	        for(j=0;j<5;j++)
+	        {
+	           System.out.print(j+1);
+	        }
+	       System.out.print("\n");
+	    }
+	   
+	}
+}
