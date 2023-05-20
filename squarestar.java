72f91fb06a18087f761be12fd8855a5fb153adfe
yagnapraneeth <119563035+yagnapraneeth@users.noreply.github.com>
2023-05-20 14:35:40 +0530

    Create squarestar.java

---
+++ squarestar.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    int i;int j;
+	    for(i=1;i<=5;i++)
+	    {
+	        for(j=1;j<=5;j++)
+	        {
+	            System.out.print("*");
+	        }
+	         System.out.print("\n");
+	    }
+	    
+	}
+}
