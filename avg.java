df3fcb7bfcc468d9f0e57408b3891c2df45b6837
yagnapraneeth <119563035+yagnapraneeth@users.noreply.github.com>
2023-05-20 11:32:37 +0530

    Create avg.java

---
+++ avg.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		double avg=(double)((a+b+c)/3);
+		System.out.println(String.format("%3f",avg));
+}
+}
