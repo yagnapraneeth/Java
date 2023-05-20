a86169c2272d6f95a269e4676bbece373982997b
yagnapraneeth <119563035+yagnapraneeth@users.noreply.github.com>
2023-05-20 14:31:49 +0530

    Create *.java

---
+++ *.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    int i;int j;
+	    Scanner sc=new Scanner(System.in);
+	     System.out.println("enter number of stars");
+	    int n=sc.nextInt();
+	    for(i=0;i<n;i++){
+	        for(j=0;j<=i;j++){
+	            System.out.print("*");
+	        }
+	         System.out.print("\n");
+	    }
+	}
+}
